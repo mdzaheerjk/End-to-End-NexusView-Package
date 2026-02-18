0d15251f28eea563e0b562508733b559cf7c91d9
zaheer <info.zaheerjk@gmail.com>
2026-02-18 14:48:28 +0530

    code removed

--- test.py
+++
@@ -1,7 +0,0 @@
-from NexusViewPro.logger import logger
-from NexusViewPro.custom_exception import InvalidURLException
-
-try:
-    raise InvalidURLException()
-except Exception as e:
-    logger.error(f"An error occurred: {e}")
